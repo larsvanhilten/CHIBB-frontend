90a0b38cdc182ed6d418d7bfbf5a3326e9d53462
Lars van Hilten <larsvh8@gmail.com>
2017-03-21 09:15:48 +0100

    Add: images

--- client/app/config.js
+++ client/app/config.js
@@ -6,7 +6,7 @@ const config = ($locationProvider, $stateProvider, $urlRouterProvider, $httpProv
   'ngInject';
 
   $locationProvider.html5Mode(true).hashPrefix('!');
-  $urlRouterProvider.otherwise('/loginq');
+  $urlRouterProvider.otherwise('/login');
 
   // Add http interceptors
   each(['endpointInjector', 'bodyCleaningInjector'], i => $httpProvider.interceptors.push(i));
