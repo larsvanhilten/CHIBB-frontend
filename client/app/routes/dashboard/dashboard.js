9d3c5f2d49c63f71f7f5eb240fc649510596d34b
Lars van Hilten <larsvh8@gmail.com>
2017-04-17 20:42:51 +0200

    Add: sensor graphs

--- client/app/routes/dashboard/dashboard.js
+++ client/app/routes/dashboard/dashboard.js
@@ -1,12 +1,49 @@
 import angular from 'angular';
 import routeWrap from 'ng-component-routing';
 import template from './dashboard.html';
+import _ from 'lodash';
+import Chart from 'chart.js/src/chart.js';
+import chartjs from 'angular-chart.js';
 import './dashboard.scss';
 
-const controller = function() {
+const controller = function($scope, Sensors) {
   'ngInject';
 
-  this.name = 'dashboard';
+  this.wind = {};
+  this.wind.options = {
+    title: {
+      display: true,
+      text: 'Wind'
+    }
+  };
+  this.wind.x = [];
+  this.wind.y = [];
+  Sensors.get('wind')
+  .then(readings => {
+    _.map(readings, reading => {
+      this.wind.y.push(reading.reading);
+      this.wind.x.push(reading.timestamp);
+    });
+  });
+
+  this.rainfall = {};
+  this.rainfall.options = {
+    title: {
+      display: true,
+      text: 'Rainfall'
+    }
+  };
+
+  this.rainfall.x = [];
+  this.rainfall.y = [];
+  Sensors.get('rainfall')
+  .then(readings => {
+    _.map(readings, reading => {
+      this.rainfall.y.push(reading.reading);
+      this.rainfall.x.push(reading.timestamp);
+    });
+  });
+
 };
 
 const dashboardComponent = {
@@ -23,5 +60,6 @@ const dashboardComponent = {
   controllerAs: 'vm'
 };
 
-routeWrap(angular).module('app.routes.dashboard', []).route('dashboard', dashboardComponent);
+routeWrap(angular).module('app.routes.dashboard', ['chart.js'])
+.route('dashboard', dashboardComponent);
 export default dashboardComponent;
