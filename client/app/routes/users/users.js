ace6244fb0f6806c5b1c7e70dc18d86578293d41
Lars van Hilten <larsvh8@gmail.com>
2017-04-30 18:35:49 +0200

    Add: Users table to Users route

--- client/app/routes/users/users.js
+++ client/app/routes/users/users.js
@@ -3,10 +3,31 @@ import routeWrap from 'ng-component-routing';
 import template from './users.html';
 import './users.scss';
 
-const controller = function() {
+const controller = function(Users) {
   'ngInject';
 
-  this.name = 'users';
+  this.column = {
+    field: 'name',
+    reverse: false
+  };
+
+  this.setOrder = column => {
+    if(this.column.field === column) {
+      this.column.reverse = !this.column.reverse;
+    } else {
+      this.column = {
+        field: column,
+        reverse: false
+      };
+    }
+  };
+
+  Users.getAll()
+  .then(users => {
+    this.users = users;
+  })
+  .catch(error => {});
+
 };
 
 const usersComponent = {
