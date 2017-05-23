87a6c78c85e641e235e757c4f1ce8a6e2985aa52
Lars van Hilten <larsvh8@gmail.com>
2017-05-23 14:59:27 +0200

    Changed: sensor page to contain CRUD 'brokers' table

--- client/app/routes/sensors/sensors.js
+++ client/app/routes/sensors/sensors.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import routeWrap from 'ng-component-routing';
 import template from './sensors.html';
 import './sensors.scss';
+import _ from 'lodash';
 
 const controller = function(Sensors, Brokers) {
   'ngInject';
@@ -12,30 +13,100 @@ const controller = function(Sensors, Brokers) {
   .then(statusses => {
     this.middle = statusses.length / 2;
     this.statusses = statusses;
-
   });
 
-  this.broker = {};
-  this.error = null;
-  this.info = null;
+  this.editBroker = {};
+  this.isEditMode = true;
+  this.editError = null;
 
+  this.brokerModal = broker => {
+    if(broker) {
+      this.isEditMode = true;
+      this.editBroker = _.clone(broker);
+    }else {
+      this.isEditMode = false;
+    }
+    this.showSensorModal = true;
+  };
   this.addBroker = () => {
     this.error = null;
-    this.info = null;
+    this.editError = null;
 
-    this.broker.port = parseInt(this.broker.port, 10);
+    this.editBroker.port = parseInt(this.editBroker.port, 10);
 
-    Brokers.add(this.broker)
+    Brokers.add(this.editBroker)
     .then(() => {
-      const form = document.getElementsByClassName('broker-form')[0];
-      form.reset();
-      this.info = 'Broker has been added!';
+      this.brokers.push(this.editBroker);
+      this.showSensorModal = false;
     })
     .catch(error => {
-      this.error = error;
+      this.editError = error;
     });
   };
+  this.updateBroker = () => {
+    this.editError = '';
+    const originalBroker = _.find(this.brokers, broker => broker._id === this.editBroker._id);
+    const updated = _.pickBy(this.editBroker, (value, key) => value !== originalBroker[key]);
 
+    if(updated.port) {
+      updated.port = parseInt(updated.port, 10);
+    }
+
+    if(_.isEmpty(updated)) {
+      this.showSensorModal = false;
+    } else {
+      updated._id = originalBroker._id;
+      Brokers.update(updated)
+      .then(broker => {
+        _.assign(originalBroker, broker);
+        this.showSensorModal = false;
+      })
+      .catch(error => {
+        this.editError = error;
+      });
+    }
+  };
+
+  this.showSensorModal = false;
+  this.error = null;
+  this.headers = ['name', 'url', 'port', 'channel'];
+  this.brokers = [];
+  this.column = {
+    field: 'name',
+    reverse: false
+  };
+
+  Brokers.getAll()
+  .then(brokers => {
+    this.brokers = brokers;
+  })
+  .catch(err => {
+    this.error = err;
+  });
+
+  this.setOrder = column => {
+    if(this.column.field === column) {
+      this.column.reverse = !this.column.reverse;
+    } else {
+      this.column = {
+        field: column,
+        reverse: false
+      };
+    }
+  };
+
+  this.deleteBroker = broker => {
+    if(confirm('Are you sure you want to delete this broker?')) {
+      Brokers.delete(broker._id)
+      .then(() => {
+        const index = this.brokers.indexOf(broker);
+        this.brokers.splice(index, 1);
+      })
+      .catch(err => {
+        this.editError = err;
+      });
+    }
+  };
 };
 
 const sensorsComponent = {
--- client/app/services/Brokers.js
+++ client/app/services/Brokers.js
@@ -3,6 +3,18 @@ import angular from 'angular';
 const BrokersService = function($q, $http) {
   'ngInject';
 
+  this.getAll = () => {
+    const defer = $q.defer();
+
+    $http.get('/brokers')
+    .then(brokers => {
+      defer.resolve(brokers);
+    })
+    .catch(err => defer.reject(err.message));
+
+    return defer.promise;
+  };
+
   this.add = broker => {
     const defer = $q.defer();
 
@@ -15,6 +27,26 @@ const BrokersService = function($q, $http) {
     return defer.promise;
   };
 
+  this.update = broker => {
+    const defer = $q.defer();
+
+    $http.put(`/brokers/${broker._id}`, broker)
+    .then(updatedBroker => defer.resolve(updatedBroker))
+    .catch(err => defer.reject(err));
+
+    return defer.promise;
+  };
+  this.delete = id => {
+    const defer = $q.defer();
+
+    $http.delete(`/brokers/${id}`)
+    .then(() => {
+      defer.resolve();
+    })
+    .catch(err => defer.reject(err.message));
+
+    return defer.promise;
+  };
 };
 
 angular.module('app.services.Brokers', []).service('Brokers', BrokersService);
