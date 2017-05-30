0182e2f3616eac7cecd0c246a7fa142035f6ec30
Lars van Hilten <larsvh8@gmail.com>
2017-05-30 18:19:07 +0200

    Changed: Dashboard to analyse two sensors

--- client/app/routes/dashboard/dashboard.js
+++ client/app/routes/dashboard/dashboard.js
@@ -3,6 +3,7 @@ import routeWrap from 'ng-component-routing';
 import template from './dashboard.html';
 import './dashboard.scss';
 import _ from 'lodash';
+import moment from 'moment';
 
 const controller = function(Sensors, $scope) {
   'ngInject';
@@ -32,19 +33,30 @@ const controller = function(Sensors, $scope) {
   })
   .catch(() => {});
 
-  $scope.$watch('vm.sensorOne', () => {
-    if(this.sensorOne) {
-      Sensors.get(this.sensorOne)
-      .then(data => {
-        _.map(data, reading => {
-          this.chart.config.datasets[0].dataY.push(reading.reading);
-          this.chart.config.dataX.push(reading.timestamp);
-          this.chart.config.loaded = true;
-        });
-      })
-      .catch(error => console.log(error));
-    }
-  });
+  $scope.$watchGroup(
+    ['vm.sensorOne', 'vm.sensorTwo',
+      'vm.chartOne', 'vm.chartTwo',
+      'vm.dateFrom', 'vm.dateTo'],
+    () => {
+      if(this.sensorOne && this.sensorTwo) {
+        if(this.chartOne && this.chartTwo) {
+          if(this.dateFrom && this.dateTo) {
+            const type = _.lowercase(this.sensorOne);
+            const fromUnix = moment(this.dateFrom, 'DD-MM-YYYY').valueOf() / 1000;
+            const toUnix = moment(this.dateTo, 'DD-MM-YYYY').valueOf() / 1000;
+            Sensors.getBetweenTime(type, fromUnix, toUnix)
+            .then(data => {
+              _.map(data, reading => {
+                this.chart.config.datasets[0].dataY.push(reading.reading);
+                this.chart.config.dataX.push(reading.timestamp);
+              });
+              this.chart.config.loaded = true;
+            })
+            .catch(error => console.log(error));
+          }
+        }
+      }
+    });
 
 };
 
--- client/app/services/Sensors.js
+++ client/app/services/Sensors.js
@@ -21,6 +21,25 @@ const SensorsService = function($http, $q) {
     return defer.promise;
   };
 
+  this.getBetweenTime = (type, from, to) => {
+    const defer = $q.defer();
+    $http.get(`/sensors/${type}/${from}/${to}`)
+    .then(readings => {
+      console.log(`/sensors/${type}/${from}/${to}`);
+      console.log(readings);
+      if(readings[0]) {
+        _.map(readings, reading => {
+          reading.timestamp = new Date(reading.timestamp * 1000);
+        });
+      }
+      defer.resolve(readings);
+    })
+    .catch(err =>
+      defer.reject(err)
+    );
+    return defer.promise;
+  };
+
   this.getStatusses = () => {
     const defer = $q.defer();
     $http.get(`/sensors`)
