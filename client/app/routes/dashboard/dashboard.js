da73616c154792d1b99b13645b3012ef520c265b
Lars van Hilten <larsvh8@gmail.com>
2017-05-30 21:24:38 +0200

    Add: bar chart to dashboard

--- client/app/routes/dashboard/dashboard.js
+++ client/app/routes/dashboard/dashboard.js
@@ -9,10 +9,25 @@ const controller = function(Sensors, $scope) {
   'ngInject';
 
   this.sensors = [];
-  this.charts = ['Line-chart'];
-  this.chart = {};
-  this.chart.config = {
+  this.charts = ['Line', 'Bar'];
+
+  this.configOne = {
     loaded: false,
+    type: 'line',
+    title: 'Wind readings',
+    labelY: 'Wind speed',
+    labelX: 'Time',
+    dataX: [],
+    datasets: [
+      {
+        label: 'Wind',
+        dataY: []
+      }
+    ]
+  };
+  this.configTwo = {
+    loaded: false,
+    type: 'line',
     title: 'Wind readings',
     labelY: 'Wind speed',
     labelX: 'Time',
@@ -34,12 +49,16 @@ const controller = function(Sensors, $scope) {
   .catch(() => {});
 
   $scope.$watchGroup(
-    ['vm.sensorOne', 'vm.sensorTwo',
-      'vm.chartOne', 'vm.chartTwo',
+    ['vm.sensorOne',
+      'vm.chartOne',
       'vm.dateFrom', 'vm.dateTo'],
     () => {
-      if(this.sensorOne && this.sensorTwo) {
-        if(this.chartOne && this.chartTwo) {
+      if(this.sensorOne) {
+        this.configOne.title = `${this.sensorOne} readings`;
+        this.configOne.labelY = `${this.sensorOne}`;
+        this.configOne.datasets[0].label = `${this.sensorOne}`;
+        if(this.chartOne) {
+          this.configOne.type = _.lowercase(this.chartOne);
           if(this.dateFrom && this.dateTo) {
             const type = _.lowercase(this.sensorOne);
             const fromUnix = moment(this.dateFrom, 'DD-MM-YYYY').valueOf() / 1000;
@@ -47,10 +66,39 @@ const controller = function(Sensors, $scope) {
             Sensors.getBetweenTime(type, fromUnix, toUnix)
             .then(data => {
               _.map(data, reading => {
-                this.chart.config.datasets[0].dataY.push(reading.reading);
-                this.chart.config.dataX.push(reading.timestamp);
+                this.configOne.datasets[0].dataY.push(reading.reading);
+                this.configOne.dataX.push(reading.timestamp);
               });
-              this.chart.config.loaded = true;
+              this.configOne.loaded = true;
+            })
+            .catch(error => console.log(error));
+          }
+        }
+      }
+    });
+
+  $scope.$watchGroup(
+    ['vm.sensorTwo',
+      'vm.chartTwo',
+      'vm.dateFrom', 'vm.dateTo'],
+    () => {
+      if(this.sensorTwo) {
+        this.configTwo.title = `${this.sensorTwo} readings`;
+        this.configTwo.labelY = `${this.sensorTwo}`;
+        this.configTwo.datasets[0].label = `${this.sensorTwo}`;
+        if(this.chartTwo) {
+          this.configTwo.type = _.lowercase(this.chartTwo);
+          if(this.dateFrom && this.dateTo) {
+            const type = _.lowercase(this.sensorTwo);
+            const fromUnix = moment(this.dateFrom, 'DD-MM-YYYY').valueOf() / 1000;
+            const toUnix = moment(this.dateTo, 'DD-MM-YYYY').valueOf() / 1000;
+            Sensors.getBetweenTime(type, fromUnix, toUnix)
+            .then(data => {
+              _.map(data, reading => {
+                this.configTwo.datasets[0].dataY.push(reading.reading);
+                this.configTwo.dataX.push(reading.timestamp);
+              });
+              this.configTwo.loaded = true;
             })
             .catch(error => console.log(error));
           }
